4c493cea1c29c61a9e46b35536229421facc3e30
Anna Rudy <annarudy04@gmail.com>
2020-08-10 12:43:04 -0500

    restructuring repo to fit more classes

--- ExploratoryDataAnalysis/Week4Project/create_plots.R
+++
@@ -1,7 +0,0 @@
-# create all 4 plots
-plot_nums <- 1:6
-for (plot in plot_nums){
-  plotname <- paste0("plot", plot)
-  print(paste("Creating", plotname, "..."))
-  source(paste0("plot_files/plot", plot, ".R"))
-}
